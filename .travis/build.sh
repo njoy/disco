b198c7bfd4316f2198c15b101a0ed871803e6cbe
Austin McCartney <amccartney@lanl.gov>
2016-10-14 10:50:09 -0600

    travis on my branch

---
+++ .travis/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+  echo "Running on linux"
+  if [ $CXX = "g++" ]
+  then 
+    echo "Running g++"
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+    sudo update-alternatives --config gcc
+  fi;
+fi
+
+./fetch_subprojects.py
+mkdir build
+cd build
+cmake -D build_type=$build_type \
+      -D static_libraries=$static_libraries \
+      -D appended_flags=$appended_flags ..
+make -j2
+export COMPILATION_FAILURE=$?
+ctest --output-on-failure -j2
+export TEST_FAILURE=$?
+if [ "$build_type" = "coverage" ]
+then
+    pip install --user cpp-coveralls
+    echo "loading coverage information"
+    coveralls  --exclude "/usr/include/" --exclude-pattern ".*subprojects.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
+fi
+if [ $COMPILATION_FAILURE -ne 0 ] || [ $TEST_FAILURE -ne 0 ];
+then
+    exit 1
+else
+    exit 0
+fi
