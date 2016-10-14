984d814f1f8abb795dc0f1ff303e6867a84d7bf6
Austin McCartney <amccartney@lanl.gov>
2016-10-14 15:49:41 -0600

    more reference work

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+if [ "$CC" = "gcc" ]; then; export VERSION=-6; fi;
+if [ "$CC" = "clang" ]; then; export VERSION=-3.8; fi;
 
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
   sudo update-alternatives --install /usr/bin/$CC $CC /usr/bin/$CC$VERSION 90 --slave /usr/bin/$CXX $CXX /usr/bin/$CC$VERSION
