8f4ba24c9ee909c563ea3c78f88be8ffaad31aba
Austin McCartney <amccartney@lanl.gov>
2016-10-15 11:49:53 -0600

    LLVMgold wasn't where I thought. let's poke arounf

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ls /usr/include/c++
-
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 --slave /usr/bin/g++ g++ /usr/bin/g++-6
   sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 90 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-3.8
@@ -9,6 +7,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo update-alternatives --config clang
   export PATH=/usr/bin:$PATH
   if [ "$CXX" = "clang++" ]; then
+    find / -iname LLVMgold.so
     sudo ln -sf /usr/lib/llvm-3.4/lib/LLVMgold.so /usr/lib/LLVMgold.so
     sudo bash -c "echo /usr/lib/llvm-3.4/lib >> /etc/ld.so.conf"
     cat /etc/ld.so.conf
@@ -18,25 +17,25 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   fi;
 fi
 
-./fetch_subprojects.py
-mkdir build
-cd build
-cmake -D build_type=$build_type \
-      -D static_libraries=$static_libraries \
-      -D appended_flags="$appended_flags" ..
-make -j2
-export COMPILATION_FAILURE=$?
-ctest --output-on-failure -j2
-export TEST_FAILURE=$?
-if [ "$build_type" = "coverage" ]
-then
-    pip install --user cpp-coveralls
-    echo "loading coverage information"
-    coveralls  --exclude "/usr/include/" --exclude-pattern ".*subprojects.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
-fi
-if [ $COMPILATION_FAILURE -ne 0 ] || [ $TEST_FAILURE -ne 0 ];
-then
-    exit 1
-else
-    exit 0
-fi
+# ./fetch_subprojects.py
+# mkdir build
+# cd build
+# cmake -D build_type=$build_type \
+#       -D static_libraries=$static_libraries \
+#       -D appended_flags="$appended_flags" ..
+# make -j2
+# export COMPILATION_FAILURE=$?
+# ctest --output-on-failure -j2
+# export TEST_FAILURE=$?
+# if [ "$build_type" = "coverage" ]
+# then
+#     pip install --user cpp-coveralls
+#     echo "loading coverage information"
+#     coveralls  --exclude "/usr/include/" --exclude-pattern ".*subprojects.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
+# fi
+# if [ $COMPILATION_FAILURE -ne 0 ] || [ $TEST_FAILURE -ne 0 ];
+# then
+#     exit 1
+# else
+#     exit 0
+# fi
