2248625240b2e30ccda952abf72544d019025155
Austin McCartney <amccartney@lanl.gov>
2016-10-15 11:40:43 -0600

    LLVMgold missing from llvm-3.8 ubuntu package. trying to use the 3.4 one

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,10 +9,10 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo update-alternatives --config clang
   export PATH=/usr/bin:$PATH
   if [ "$CXX" = "clang++" ]; then
-    sudo ln -sf /usr/lib/llvm-3.8/lib/LLVMgold.so /usr/lib/LLVMgold.so
-    sudo bash -c "echo /usr/lib/llvm-3.8/lib >> /etc/ld.so.conf"
+    sudo ln -sf /usr/lib/llvm-3.4/lib/LLVMgold.so /usr/lib/LLVMgold.so
+    sudo bash -c "echo /usr/lib/llvm-3.4/lib >> /etc/ld.so.conf"
     cat /etc/ld.so.conf
-    ls /usr/lib/llvm-3.8/lib
+    ls /usr/lib/llvm-3.4/lib
     sudo ldconfig
     export appended_flags=$appended_flags"-stdlib=libstdc++ -B/usr/lib/gold-ld"
   fi;
