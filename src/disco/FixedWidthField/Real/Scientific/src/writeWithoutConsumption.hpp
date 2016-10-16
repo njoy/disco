db92e53b8e036a75945b2c3fe74722fecf9d5336
Austin McCartney <austin.p.mccartney@gmail.com>
2016-10-16 15:39:36 -0600

    more mingw hacking

--- src/disco/FixedWidthField/Real/Scientific/src/writeWithoutConsumption.hpp
+++ src/disco/FixedWidthField/Real/Scientific/src/writeWithoutConsumption.hpp
@@ -10,12 +10,10 @@ writeWithoutConsumption
   const auto exponentEnd = mantissaIt + consumedCharacters;
   auto exponentIt = exponentEnd - bufferExponentPositions - 2;
   const auto mantissaEnd = exponentIt;
-
-  int exponentDigitDifference = int( exponentDigits ) - bufferExponentPositions;
   #ifdef __MINGW32__
   exponentDigitDifference -= 1;
   #endif
-
+  int exponentDigitDifference = int( exponentDigits ) - bufferExponentPositions;
   while ( exponentDigitDifference++ < 0 ){ *it++ = ' '; }
   while ( exponentDigitDifference-- > 1 ){ ++mantissaIt; }
   while ( mantissaIt != mantissaEnd ){ *it++ = *mantissaIt++; }
@@ -23,6 +21,9 @@ writeWithoutConsumption
   *it++ = *exponentIt++; // exponent sign
   exponentDigitDifference--;
   exponentDigitDifference = int( exponentDigits ) - bufferExponentPositions;
+  #ifdef __MINGW32__
+  exponentDigitDifference -= 1;
+  #endif
   while ( exponentDigitDifference++ < 0 ){ ++exponentIt; }
   while ( exponentDigitDifference-- > 1 ){ *it++ = '0'; }
   while ( exponentIt != exponentEnd ){ *it++ = *exponentIt++; }
