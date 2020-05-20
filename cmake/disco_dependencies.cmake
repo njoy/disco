53f99cf7e42fbff00d98d443ebf417cb2fca8ef8
Nathan Gibson <ngibson@lanl.gov>
2020-05-20 15:31:17 -0600

    Adding missed files

---
+++ cmake/disco_dependencies.cmake
@@ -0,0 +1,25 @@
+include( FetchContent )
+
+
+########################################################################
+# Forward declarations
+########################################################################
+
+
+########################################################################
+# Declare project dependencies
+########################################################################
+
+FetchContent_Declare( catch-adapter
+    GIT_REPOSITORY  http://github.com/njoy/catch-adapter
+    GIT_TAG         origin/build/fetchcontent
+    )
+
+
+########################################################################
+# Load dependencies
+########################################################################
+
+FetchContent_MakeAvailable(
+    catch-adapter
+    )
