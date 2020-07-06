f3938fa13cfa9081007f52b986e9ab57ab10e07a
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-07-06 15:20:02 -0600

    Adding precision (when possible) to our ENDF writer.

--- src/disco/FixedWidthField/Integer/test/write.test.cpp
+++ src/disco/FixedWidthField/Integer/test/write.test.cpp
@@ -9,22 +9,29 @@
 SCENARIO("Integer write", "[Integer], [write]"){
   {
     std::vector< std::pair< int, std::string > >
-      testSet = { { 0, "          0" },
-                  { 10, "         10" },
-                  { std::numeric_limits<int>::max(), " 2147483647"},
-                  { std::numeric_limits<int>::min() + 1, "-2147483647"} };
+      testSet = { 
+        { 0, "          0" },
+        { 10, "         10" },
+        { std::numeric_limits<int>::max(), " 2147483647"},
+        { std::numeric_limits<int>::min() + 1, "-2147483647"},
+        { +1.00000010e+9, " 1000000100" },
+        { +1.00000010e+8, "  100000010" },
+        { -1.00000010e+8, " -100000010" },
+        { -1.00000010e+9, "-1000000100" },
+      };
     for ( auto& pair : testSet ){
       std::string buffer("");
       auto it = std::back_inserter(buffer);
       njoy::disco::Integer< 11 >::write( pair.first, it );
       std::cout << std::numeric_limits<int>::min() + 1 << std::endl;
-      REQUIRE(buffer == pair.second);
+      CHECK(buffer == pair.second);
     }
+
     std::string buffer("");
     auto it = std::back_inserter(buffer);
     std::uint64_t ul = 1;
     njoy::disco::Integer< 11 >::write( ul, it );
-    REQUIRE(buffer == "          1");
+    CHECK(buffer == "          1");
   }
   {
     std::vector< std::pair< int, std::string > >
@@ -34,19 +41,20 @@ SCENARIO("Integer write", "[Integer], [write]"){
       std::string buffer("");
       auto it = std::back_inserter(buffer);
       njoy::disco::Integer< 11, 4 >::write( pair.first, it );
-      REQUIRE(buffer == pair.second);
+      CHECK(buffer == pair.second);
     }
     std::string buffer("");
     auto it = std::back_inserter(buffer);
     njoy::disco::Integer< 8 >::write( std::numeric_limits<int>::max(), it );
-    REQUIRE(buffer == "********");
+    CHECK(buffer == "********");
     buffer = "";
     it = std::back_inserter(buffer);
     njoy::disco::Integer< 11, 4 >::write( std::numeric_limits<int>::max(), it );
-    REQUIRE(buffer == " 2147483647");
+    CHECK(buffer == " 2147483647");
   }
+
   std::string buffer("");
   auto it = std::back_inserter(buffer);
   njoy::disco::Integer< 11 >::write( it );
-  REQUIRE(buffer == "           ");
+  CHECK(buffer == "           ");
 }
--- src/disco/FixedWidthField/Real/ENDF/src/write.hpp
+++ src/disco/FixedWidthField/Real/ENDF/src/write.hpp
@@ -10,22 +10,68 @@ write( Representation real, Iterator& it ){
   } else if ( isInvalid( real ) ){
     writeInvalid( it );
   } else {
-    std::array< char, w + 3 > buffer;
-    auto greedyExponent = requiresTwoDigitExponent( real );
-    const auto length = w + 2;
-    const auto decimalDigits = 6 - greedyExponent;
-    int consumedCharacters = snprintf( &buffer[0], length + 1, "%*.*E",
-                                       length, decimalDigits, real );
-    if ( consumedCharacters < 0 ){
-      throw std::exception();
+
+    // Minimal exponent size is 2 (one sign and one digit)
+    constexpr int MIN_EXPWIDTH = 2;
+    // Minimal exponent for which we can use notation
+    constexpr int MIN_EXPONENT = -2;
+
+    // Decompose real
+    double significand{ real };
+    int exponent{ 0 };
+    int expWidth{ MIN_EXPWIDTH };
+    if( real != 0.0 ){
+      exponent = std::floor( std::log10( std::abs( real ) ) );
+      if( 0 < exponent ){
+        significand /= std::pow( 10.0, exponent );
+        expWidth += std::floor( std::log10( std::abs( exponent ) ) );
+      } else if( 0 > exponent ){
+        significand *= std::pow( 10.0, -exponent );
+        expWidth += std::floor( std::log10( std::abs( exponent ) ) );
+      }
     }
-    auto mantissaIt = buffer.begin() + greedyExponent;
-    auto bufferEnd = mantissaIt + length - greedyExponent;
-    auto sign = bufferEnd - 3;
-    auto mantissaEnd = sign - 1;
-    auto exponentIt = bufferEnd - 1 - greedyExponent;
-    while( mantissaIt != mantissaEnd ){ *it++ = *mantissaIt++; }
-    *it++ = *sign;
-    while( exponentIt != bufferEnd ){ *it++ = *exponentIt++; }
+
+    unsigned int width = w - expWidth;
+    unsigned int precision = width - 3;
+    bool fixed = false;
+
+    if( ( MIN_EXPONENT < exponent ) and ( exponent < ( w - 1 ) ) ){
+      double max = std::pow( 10.0, expWidth );
+      double remainder = std::abs( significand ) * std::pow( 10.0, precision );
+      remainder -= std::floor( remainder );
+      remainder *= max;
+
+      if( ( 0.5 <= remainder ) and ( 0.5 <= ( max - remainder ) ) ){
+         fixed = true;
+         precision += expWidth;
+         width += expWidth;
+        
+         if( exponent > static_cast< int >( precision ) ){
+           precision = 0;
+         } else if( 0 < exponent ) {
+           precision -= exponent;
+         }
+
+      }
+    }
+
+    std::ostringstream buffer;
+    buffer << std::setw(width) << std::fixed << std::showpoint
+           << std::right << std::setprecision( precision );
+    if( fixed ){
+      if( 0 == precision ){
+        buffer << static_cast< int >( real );
+      }
+      else {
+        buffer << real;
+      }
+    }
+    else {
+      buffer << significand << std::showpos << exponent;
+    }
+
+    for( auto b : buffer.str() ){
+      *it++ = b;
+    }
   }
 }
