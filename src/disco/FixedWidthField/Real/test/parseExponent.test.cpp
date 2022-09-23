dc7973a9e989ef49b9039262a8755ad12f9bfb3b
Wim Haeck <whaeck@gmail.com>
2022-09-23 15:42:48 -0600

    Added testing exceptions

--- src/disco/FixedWidthField/Real/test/parseExponent.test.cpp
+++ src/disco/FixedWidthField/Real/test/parseExponent.test.cpp
@@ -1,5 +1,5 @@
+#include <iterator>
 #include <string>
-#include <vector>
 
 #include "disco.hpp"
 #include "catch.hpp"
@@ -13,33 +13,63 @@ SCENARIO( "Real parse exponent" ) {
     return njoy::disco::Real<6>::parseExponent( iter, position );
   };
 
-  CHECK( +123 == parse( "+123" ) );
-  CHECK( +123 == parse( "+ 123" ) );
-  CHECK( +123 == parse( "E+123" ) );
-  CHECK( +123 == parse( "E+ 123" ) );
-  CHECK( +123 == parse( "E123" ) );
-  CHECK( +123 == parse( "E 123" ) );
-  CHECK( +123 == parse( "e+123" ) );
-  CHECK( +123 == parse( "e+ 123" ) );
-  CHECK( +123 == parse( "e123" ) );
-  CHECK( +123 == parse( "e 123" ) );
-  CHECK( +123 == parse( "D+123" ) );
-  CHECK( +123 == parse( "D+ 123" ) );
-  CHECK( +123 == parse( "D123" ) );
-  CHECK( +123 == parse( "D 123" ) );
-  CHECK( +123 == parse( "d+123" ) );
-  CHECK( +123 == parse( "d+ 123" ) );
-  CHECK( +123 == parse( "d123" ) );
-  CHECK( +123 == parse( "d 123" ) );
+  THEN( "valid exponent strings can be read" )
+  {
+    CHECK( +123 == parse( "+123" ) );
+    CHECK( +123 == parse( "+ 123" ) );
+    CHECK( +123 == parse( "E+123" ) );
+    CHECK( +123 == parse( "E+ 123" ) );
+    CHECK( +123 == parse( "E123" ) );
+    CHECK( +123 == parse( "E 123" ) );
+    CHECK( +123 == parse( "e+123" ) );
+    CHECK( +123 == parse( "e+ 123" ) );
+    CHECK( +123 == parse( "e123" ) );
+    CHECK( +123 == parse( "e 123" ) );
+    CHECK( +123 == parse( "D+123" ) );
+    CHECK( +123 == parse( "D+ 123" ) );
+    CHECK( +123 == parse( "D123" ) );
+    CHECK( +123 == parse( "D 123" ) );
+    CHECK( +123 == parse( "d+123" ) );
+    CHECK( +123 == parse( "d+ 123" ) );
+    CHECK( +123 == parse( "d123" ) );
+    CHECK( +123 == parse( "d 123" ) );
 
-  CHECK( -123 == parse( "-123" ) );
-  CHECK( -123 == parse( "- 123" ) );
-  CHECK( -123 == parse( "E-123" ) );
-  CHECK( -123 == parse( "E- 123" ) );
-  CHECK( -123 == parse( "e-123" ) );
-  CHECK( -123 == parse( "e- 123" ) );
-  CHECK( -123 == parse( "D-123" ) );
-  CHECK( -123 == parse( "D- 123" ) );
-  CHECK( -123 == parse( "d-123" ) );
-  CHECK( -123 == parse( "d- 123" ) );
+    CHECK( -123 == parse( "-123" ) );
+    CHECK( -123 == parse( "- 123" ) );
+    CHECK( -123 == parse( "E-123" ) );
+    CHECK( -123 == parse( "E- 123" ) );
+    CHECK( -123 == parse( "e-123" ) );
+    CHECK( -123 == parse( "e- 123" ) );
+    CHECK( -123 == parse( "D-123" ) );
+    CHECK( -123 == parse( "D- 123" ) );
+    CHECK( -123 == parse( "d-123" ) );
+    CHECK( -123 == parse( "d- 123" ) );
+  }
+
+  THEN( "invalid exponent strings cause an exception" )
+  {
+    CHECK_THROWS( parse( "--123" ) );
+    CHECK_THROWS( parse( "-+123" ) );
+    CHECK_THROWS( parse( "++123" ) );
+    CHECK_THROWS( parse( "+-123" ) );
+    CHECK_THROWS( parse( "E--123" ) );
+    CHECK_THROWS( parse( "E-+123" ) );
+    CHECK_THROWS( parse( "E++123" ) );
+    CHECK_THROWS( parse( "E+-123" ) );
+    CHECK_THROWS( parse( "D--123" ) );
+    CHECK_THROWS( parse( "D-+123" ) );
+    CHECK_THROWS( parse( "D++123" ) );
+    CHECK_THROWS( parse( "D+-123" ) );
+    CHECK_THROWS( parse( "E -123" ) );
+    CHECK_THROWS( parse( "E +123" ) );
+    CHECK_THROWS( parse( "E +123" ) );
+    CHECK_THROWS( parse( "E -123" ) );
+    CHECK_THROWS( parse( "D -123" ) );
+    CHECK_THROWS( parse( "D +123" ) );
+    CHECK_THROWS( parse( "D +123" ) );
+    CHECK_THROWS( parse( "D -123" ) );
+
+    CHECK_THROWS( parse( "-  a3" ) );
+    CHECK_THROWS( parse( "-    " ) );
+  }
 }
