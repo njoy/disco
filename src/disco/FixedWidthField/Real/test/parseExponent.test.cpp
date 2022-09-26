50c6a2bcf2428adb752736c56d631859952faa2e
Wim Haeck <whaeck@gmail.com>
2022-09-26 14:16:14 -0600

    Added position testing

--- src/disco/FixedWidthField/Real/test/parseExponent.test.cpp
+++ src/disco/FixedWidthField/Real/test/parseExponent.test.cpp
@@ -4,72 +4,94 @@
 #include "disco.hpp"
 #include "catch.hpp"
 
-SCENARIO( "Real parse exponent" ) {
+SCENARIO( "Real - parse exponent" ) {
 
-  auto parse = [] ( const std::string string ) {
+  auto parse = [] ( const std::string string, uint16_t& position ) {
 
     auto iter = string.begin();
-    uint16_t position = 0;
-    return njoy::disco::Real<6>::parseExponent( iter, position );
+    position = 0;
+    return njoy::disco::Real< 6 >::parseExponent( iter, position );
   };
 
-  THEN( "valid exponent strings can be read" )
+  THEN( "valid exponent strings can be readup to the correct position" )
   {
-    CHECK( +123 == parse( "+123" ) );
-    CHECK( +123 == parse( "+ 123" ) );
-    CHECK( +123 == parse( "E+123" ) );
-    CHECK( +123 == parse( "E+ 123" ) );
-    CHECK( +123 == parse( "E123" ) );
-    CHECK( +123 == parse( "E 123" ) );
-    CHECK( +123 == parse( "e+123" ) );
-    CHECK( +123 == parse( "e+ 123" ) );
-    CHECK( +123 == parse( "e123" ) );
-    CHECK( +123 == parse( "e 123" ) );
-    CHECK( +123 == parse( "D+123" ) );
-    CHECK( +123 == parse( "D+ 123" ) );
-    CHECK( +123 == parse( "D123" ) );
-    CHECK( +123 == parse( "D 123" ) );
-    CHECK( +123 == parse( "d+123" ) );
-    CHECK( +123 == parse( "d+ 123" ) );
-    CHECK( +123 == parse( "d123" ) );
-    CHECK( +123 == parse( "d 123" ) );
+    uint16_t position = 0;
 
-    CHECK( -123 == parse( "-123" ) );
-    CHECK( -123 == parse( "- 123" ) );
-    CHECK( -123 == parse( "E-123" ) );
-    CHECK( -123 == parse( "E- 123" ) );
-    CHECK( -123 == parse( "e-123" ) );
-    CHECK( -123 == parse( "e- 123" ) );
-    CHECK( -123 == parse( "D-123" ) );
-    CHECK( -123 == parse( "D- 123" ) );
-    CHECK( -123 == parse( "d-123" ) );
-    CHECK( -123 == parse( "d- 123" ) );
+    CHECK( +123 == parse( "+123", position ) );
+    CHECK( 4 == position );
+    CHECK( +123 == parse( "+ 123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "E+123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "E+ 123", position ) );
+    CHECK( 6 == position );
+    CHECK( +123 == parse( "E123", position ) );
+    CHECK( 4 == position );
+    CHECK( +123 == parse( "E 123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "e+123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "e+ 123", position ) );
+    CHECK( 6 == position );
+    CHECK( +123 == parse( "e123", position ) );
+    CHECK( 4 == position );
+    CHECK( +123 == parse( "e 123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "D+123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "D+ 123", position ) );
+    CHECK( 6 == position );
+    CHECK( +123 == parse( "D123", position ) );
+    CHECK( 4 == position );
+    CHECK( +123 == parse( "D 123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "d+123", position ) );
+    CHECK( 5 == position );
+    CHECK( +123 == parse( "d+ 123", position ) );
+    CHECK( 6 == position );
+    CHECK( +123 == parse( "d123", position ) );
+    CHECK( 4 == position );
+    CHECK( +123 == parse( "d 123", position ) );
+    CHECK( 5 == position );
+
+    CHECK( -123 == parse( "-123", position ) );
+    CHECK( 4 == position );
+    CHECK( -123 == parse( "- 123", position ) );
+    CHECK( 5 == position );
+    CHECK( -123 == parse( "E-123", position ) );
+    CHECK( 5 == position );
+    CHECK( -123 == parse( "E- 123", position ) );
+    CHECK( 6 == position );
+    CHECK( -123 == parse( "e-123", position ) );
+    CHECK( 5 == position );
+    CHECK( -123 == parse( "e- 123", position ) );
+    CHECK( 6 == position );
+    CHECK( -123 == parse( "D-123", position ) );
+    CHECK( 5 == position );
+    CHECK( -123 == parse( "D- 123", position ) );
+    CHECK( 6 == position );
+    CHECK( -123 == parse( "d-123", position ) );
+    CHECK( 5 == position );
+    CHECK( -123 == parse( "d- 123", position ) );
+    CHECK( 6 == position );
   }
 
   THEN( "invalid exponent strings cause an exception" )
   {
-    CHECK_THROWS( parse( "--123" ) );
-    CHECK_THROWS( parse( "-+123" ) );
-    CHECK_THROWS( parse( "++123" ) );
-    CHECK_THROWS( parse( "+-123" ) );
-    CHECK_THROWS( parse( "E--123" ) );
-    CHECK_THROWS( parse( "E-+123" ) );
-    CHECK_THROWS( parse( "E++123" ) );
-    CHECK_THROWS( parse( "E+-123" ) );
-    CHECK_THROWS( parse( "D--123" ) );
-    CHECK_THROWS( parse( "D-+123" ) );
-    CHECK_THROWS( parse( "D++123" ) );
-    CHECK_THROWS( parse( "D+-123" ) );
-    CHECK_THROWS( parse( "E -123" ) );
-    CHECK_THROWS( parse( "E +123" ) );
-    CHECK_THROWS( parse( "E +123" ) );
-    CHECK_THROWS( parse( "E -123" ) );
-    CHECK_THROWS( parse( "D -123" ) );
-    CHECK_THROWS( parse( "D +123" ) );
-    CHECK_THROWS( parse( "D +123" ) );
-    CHECK_THROWS( parse( "D -123" ) );
+    uint16_t position = 0;
 
-    CHECK_THROWS( parse( "-  a3" ) );
-    CHECK_THROWS( parse( "-    " ) );
+    CHECK_THROWS( parse( "-a123", position ) );
+    CHECK_THROWS( parse( "+a123", position ) );
+    CHECK_THROWS( parse( "E-a123", position ) );
+    CHECK_THROWS( parse( "E+a123", position ) );
+    CHECK_THROWS( parse( "D-a123", position ) );
+    CHECK_THROWS( parse( "D+a123", position ) );
+    CHECK_THROWS( parse( "E -123", position ) );
+    CHECK_THROWS( parse( "E +123", position ) );
+    CHECK_THROWS( parse( "D -123", position ) );
+    CHECK_THROWS( parse( "D +123", position ) );
+
+    CHECK_THROWS( parse( "-  a3", position ) );
+    CHECK_THROWS( parse( "-    ", position ) );
   }
 }
