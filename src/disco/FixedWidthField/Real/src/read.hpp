d18b59d102d400f1124cd2a14161215731cdb36b
Wim Haeck <whaeck@gmail.com>
2022-09-27 11:40:17 -0600

    Allowing for trailing white space after the fraction value, added more tests to that effect

--- src/disco/FixedWidthField/Real/src/read.hpp
+++ src/disco/FixedWidthField/Real/src/read.hpp
@@ -1,28 +1,34 @@
 template< typename Representation, typename Iterator, bool trust = true >
 static Representation
-read( Iterator& it, const Iterator& ){
+read( Iterator& it, const Iterator& ) {
+
   auto position = FixedWidthField_::whiteSpace( it );
 
-  if( FixedWidthField_::isNewline(*it, it) or FixedWidthField_::isEOF(*it) ){
+  if( FixedWidthField_::isNewline(*it, it) or FixedWidthField_::isEOF(*it) ) {
+
     return Representation(0.0);
   }
 
-  if ( position == w ){
+  if ( position == w ) {
+
     ++it;
     return Representation(0.0);
   }
 
-  const auto value = [&]{
+  const auto value = [&] {
+
     const auto sign = Real::parseSign(it, position);
 
-    if ( unlikely( position == width ) ){
+    if ( unlikely( position == width ) ) {
+
       throw std::runtime_error("cannot parse invalid real number");
     }
 
     bool baseSuccess = false;
     auto base = Real::parseBase(it, position, baseSuccess);
 
-    if ( position == width ){
+    if ( position == width ) {
+
       return Representation( sign * int64_t( base ) );
     }
 
@@ -31,47 +37,56 @@ read( Iterator& it, const Iterator& ){
     const auto fraction = Real::parseFraction( it, position, fractionSuccess );
     const auto foundDecimal = decimalPosition != position;
 
-    const auto noFractionDigits =
+    const auto numberFractionDigits =
       fractionSuccess
       * ( position - decimalPosition - (position != width - 1) );
 
-    if ( position == width ){
+    if ( position == width ) {
+
       const auto factor =
-        realExponentiation< Representation >::cache( -noFractionDigits );
+        realExponentiation< Representation >::cache( -numberFractionDigits );
       return sign * ( base + fraction * Representation(factor) );
     }
 
-    if ( unlikely( not baseSuccess && not fractionSuccess ) ){
+    if ( unlikely( not baseSuccess && not fractionSuccess ) ) {
+
       const bool succeeded =
         not foundDecimal and Real::parseInfinity( it, position );
-      if ( unlikely( not succeeded ) ){
+      if ( unlikely( not succeeded ) ) {
+
         throw std::runtime_error("cannot parse invalid real number");
       }
 
       return sign * std::numeric_limits< Representation >::infinity();
     }
 
+    // removing trailing white space between the fraction and exponent
+    position += FixedWidthField_::whiteSpace( it );
+
     const auto exponent =
-      Real::parseExponent( it, position ) - noFractionDigits;
+      Real::parseExponent( it, position ) - numberFractionDigits;
 
     if ( unlikely( exponent
-                   < std::numeric_limits< Representation >::min_exponent10 ) ){
+                   < std::numeric_limits< Representation >::min_exponent10 ) ) {
+
       return sign * 0.0;
     }
 
     if ( unlikely( exponent
-                   > std::numeric_limits< Representation >::max_exponent10 ) ){
+                   > std::numeric_limits< Representation >::max_exponent10 ) ) {
+
       return sign * std::numeric_limits< Representation >::infinity();
     }
 
     const auto factor =
-      integerExponentiation< int64_t >::cache( noFractionDigits );
+      integerExponentiation< int64_t >::cache( numberFractionDigits );
 
     return sign * int64_t( base * factor + fraction )
      * realExponentiation< Representation >::cache( exponent );
   }();
 
-  if ( unlikely( not FixedWidthField_::whiteSpace(it, position) ) ){
+  if ( unlikely( not FixedWidthField_::whiteSpace(it, position) ) ) {
+
     throw std::runtime_error("cannot parse invalid real number");
   }
 
@@ -80,6 +95,7 @@ read( Iterator& it, const Iterator& ){
 
 template< typename Iterator, bool trust = true >
 static double
-read( Iterator& it, const Iterator& end ){
+read( Iterator& it, const Iterator& end ) {
+
   return read<double>( it, end );
 }
